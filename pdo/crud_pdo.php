742f996b0e115ab8e77e830e35185f8cdd6a932a
Maykon Dias <Maykondias2001@gmail.com>
2023-01-18 22:45:17 -0300

    Update

--- pdo/crud_pdo.php
+++ pdo/crud_pdo.php
@@ -1,7 +1,7 @@
 <?php 
 use \PDO;
 
-//CONEXÃO
+//-----------------------CONEXÃO-----------------------
 try {
     $pdo = new PDO('mysql:dbname=crudpdo;host=localhost', 'root', '1234');
 } catch (PDOException $e) {
@@ -11,5 +11,32 @@ catch (Exception $e) {
     echo "Erro generico: " . $e->getMessage();
 }
 
-//INSERT
+//-----------------------INSERT-----------------------
+
+//1 Forma:
+//$res = $pdo->prepare("INSERT INTO PESSOA(nome, telefone, email) VALUES(:n, :t, :e)");
+
+// $res->bindValue(":n","Miriam");
+// $res->bindValue(":t","00000000");
+// $res->bindValue(":n","teste@gmail.com");
+// $res->execute();
+
+//$res->execute([':n'=>'Miriama',':t'=>'00000000',':e'=>'teste@gmail.com']);
+
+//$res->bindParam();
+
+//2 Forma:
+// $pdo->query("INSERT INTO PESSOA(nome, telefone, email) VALUES('Maykon','21996343812','maykondias2001@gmail.com')");
+
+//-----------------------DELETE-----------------------
+// $cmd = $pdo->prepare("DELETE FROM PESSOA WHERE ID = ?");
+// $id = 1;
+// $cmd->execute([$id]);
+
+// $cmd = $pdo->query("DELETE FROM PESSOA WHERE ID = 7");
+
+//-----------------------UPDATE-----------------------
+$cmd = $pdo->prepare("UPDATE pessoa SET email = :email, nome = :nome WHERE id = :id");
+$cmd->execute([':email' => 'Maykondias2002@gmail.com', ':nome'=>'Maykon José Dias da Silva',':id' => 8]);
+
 ?>
