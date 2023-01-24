d7d12642a2413b2e03d164802c6ddaf9eaf598fc
Maykon Dias <Maykondias2001@gmail.com>
2023-01-23 23:43:11 -0300

    Funcional - 1.0

--- crud-pessoa/classe-pessoa.php
+++ crud-pessoa/classe-pessoa.php
@@ -55,11 +55,14 @@ class Pessoa{
         return "Cadastrado com Sucesso!";
     }
 
+    
+
     public function verificador_de_cadastro($array){
-        $teste = $this->pdo->prepare('SELECT ID FROM PESSOA WHERE EMAIL = :e OR TELEFONE = :t');
+        
+            $teste = $this->pdo->prepare('SELECT ID FROM PESSOA WHERE EMAIL = :e OR TELEFONE = :t');
         
         try{
-            $teste->execute([':e' => $array['email'], ':t' => $array['telefone']]);       
+            $teste->execute([':e' => $array['email'], ':t' => $array['telefone']]);   
         }catch(PDOException $e){
             die('ERROR: ' . $e->getMessage());
         }
@@ -90,7 +93,68 @@ class Pessoa{
             die('ERROR: ' . $e->getMessage());
         }
     }
+
+    public function editar($id){
+        $cmd = $this->pdo->prepare('SELECT id,nome,telefone,email FROM PESSOA WHERE ID = :id');
+        $cmd->execute([':id' => $id]);
+
+        $res = $cmd->fetch(PDO::FETCH_ASSOC);
+        return $res;        
+
+    }
+
+    public function atualizar($array){
+        
+        $teste = $this->verificador_de_atualizacao(['email' => $array['email'], 'telefone' => $array['telefone'], 'id' => $array['id']]);
+        
+        if(!$teste){
+            return "Email ou Telefone já existe!";
+        }
+
+        $campos = array_keys($array);
+        $valores = array_pad([], count($campos), "?");
+
+        $cmd = $this->pdo->prepare("UPDATE PESSOA SET NOME = :n, TELEFONE = :t, EMAIL = :e WHERE ID = :id");
+        
+        
+        try{
+            $cmd->execute([':n' => $array['nome'],':t' => $array['telefone'],':e' => $array['email'],':id' => $array['id']]);        
+        }catch(PDOException $e){
+            die('ERROR: ' . $e->getMessage());
+        }
+        catch(Exception $e){
+            die('ERROR: ' . $e->getMessage());
+        }
+
+        return "Atualizado com Sucesso!";
+    }
+
+    public function verificador_de_atualizacao($array){
+
+            $teste = $this->pdo->prepare('SELECT ID FROM PESSOA WHERE (EMAIL = :e OR TELEFONE = :t) AND ID != :id');
+        
+        try{
+            $teste->execute([':e' => $array['email'], ':t' => $array['telefone'], ':id' => $array['id'] ]);   
+        }catch(PDOException $e){
+            die('ERROR: ' . $e->getMessage());
+        }
+        catch(Exception $e){
+            die('ERROR: ' . $e->getMessage());
+        }
+
+        $res = $teste->fetch(PDO::FETCH_ASSOC);
+       
+        
+        if($res){
+            return false;
+        }
+
+        return true;
+        
+    }
 }
+
+
 
 
 
--- crud-pessoa/index.php
+++ crud-pessoa/index.php
@@ -1,20 +1,32 @@
-<?php 
-   require_once("./classe-pessoa.php");
-    
-    $p = new Pessoa();
+<?php
+require_once("./classe-pessoa.php");
 
-    if(isset($_GET['id'])){
-        $p->excluir_pessoa(addslashes($_GET['id']));
-        header("location: index.php");
-    }
+$p = new Pessoa();
 
-    if(isset($_POST['status'])){
-        if(!empty($_POST['nome']) && !empty($_POST['telefone']) && !empty($_POST['email'])){
-            echo $p->cadastrar_pessoa(['nome' => addslashes($_POST['nome']), 'telefone' => $_POST['telefone'], 'email' => addslashes($_POST['email'])]);
-        }else{
-            echo "Preencha todos os campos";
-        }
+if (isset($_POST['atualizar'])) {
+    if (!empty($_POST['nome']) && !empty($_POST['telefone']) && !empty($_POST['email'])) {
+        echo $p->atualizar(['id' => addslashes($_POST['atualizar']),'nome' => addslashes($_POST['nome']), 'telefone' =>  addslashes($_POST['telefone']), 'email' => addslashes($_POST['email'])]);
+    } else {
+        echo "Preencha todos os campos";
+    } 
+}
+
+if (isset($_POST['editar'])) {
+    $res = $p->editar(addslashes($_POST['editar']));
+}
+
+
+if (isset($_POST['excluir'])) {
+    $p->excluir_pessoa(addslashes($_POST['excluir']));
+}
+
+if (isset($_POST['cadastrar'])) {
+    if (!empty($_POST['nome']) && !empty($_POST['telefone']) && !empty($_POST['email'])) {
+        echo $p->cadastrar_pessoa(['nome' => addslashes($_POST['nome']), 'telefone' => $_POST['telefone'], 'email' => addslashes($_POST['email'])]);
+    } else {
+        echo "Preencha todos os campos";
     }
+}
 ?>
 
 
@@ -31,45 +43,55 @@
 
 <body>
     <section id="esquerda">
-        <form action="#" method="post">
+        <form action="#" method="post" class="form-cadastro">
             <h3 id="titulo">CADASTRAR PESSOA</h3>
             <label for="nome">Nome</label>
-            <input type="text" name="nome" id="nome">
+            <input type="text" name="nome" id="nome" value="<?= $res['nome'] ?>">
             <label for="telefone">Telefone</label>
-            <input type="text" name="telefone" id="telefone">
+            <input type="text" name="telefone" id="telefone" value="<?= $res['telefone'] ?>">
             <label for="email">Email</label>
-            <input type="text" name="email" id="email">
+            <input type="text" name="email" id="email" value="<?= $res['email'] ?>">
 
-            <input type="submit" value="cadastrar" name="status">
+            <?php
+            if(isset($_POST['editar'])){
+                echo "<button type='submit' name='atualizar' value='". $res['id'] ."' class='atualizar'>atualizar</button>";
+                echo "<button type='submit' class='voltar'>voltar</button>";
+            }else{
+                echo '<input type="submit" value="cadastrar" name="cadastrar">';
+            }
+            ?>
         </form>
     </section>
+    <button type="submit"></button>
     <section id="direita">
-        <table>
-            <thead>
-                <tr>
-                    <th>NOME</th>
-                    <th>TELEFONE</th>
-                    <th colspan="2">EMAIL</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php 
-                $dados = $p->buscar_dados();
-                if(!$dados == []){
-                    foreach ($dados as $dado) {
-                        echo "<tr>";
-                        echo "<td>{$dado['NOME']}</td>";
-                        echo "<td>{$dado['TELEFONE']}</td>";
-                        echo "<td>{$dado['EMAIL']}</td>";
-                        echo "<td><a href='#' value='tudo' name='teste' class='editar_btn'>Editar</a><a href='?id=".$dado['ID']."' class='excluir_btn'>Excluir</a></td>";
-                        echo "</tr>";
+        <form action="#" method="post">
+            <table>
+                <thead>
+                    <tr>
+                        <th>NOME</th>
+                        <th>TELEFONE</th>
+                        <th colspan="2">EMAIL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $dados = $p->buscar_dados();
+                    if (!$dados == []) {
+                        foreach ($dados as $dado) {
+                            echo "<tr>";
+                            echo "<td>{$dado['NOME']}</td>";
+                            echo "<td>{$dado['TELEFONE']}</td>";
+                            echo "<td>{$dado['EMAIL']}</td>";
+                            echo "<td class=''><button type='submit' value='". $dado['ID'] ."' name='editar' class='editar_btn'>Editar</button><button type='submit' value='". $dado['ID'] ."' name='excluir' class='excluir_btn'>Excluir</button></td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='3' class='banco_vazio'>SEM DADOS</td></tr>";
                     }
-                } else{
-                    echo "<tr><td colspan='3' class='banco_vazio'>SEM DADOS</td></tr>";
-                }
-                ?>
-            </tbody>
-        </table>
+                    ?>
+                </tbody>
+            </table>
+        </form>
     </section>
 </body>
 
